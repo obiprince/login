1645a145ba84fe96caf1afaa8fe340784b71977b
Ebiem Obiora <108434981+obiprince@users.noreply.github.com>
2022-08-15 11:14:02 +0100

    Add files via upload

---
+++ form2.php
@@ -0,0 +1,16 @@
+<html>
+<head><title>LOGIN PAGE</title></head>
+
+<body>
+................<i><b><em>LOGIN<i><b><em>................
+<body style="background-color:lightblue;">
+
+<form action="process.php" Method="POST">
+<input type="text" name="username"placeholder="USERNAME"><br>
+<input type="password" name="password"placeholder="PASSWORD"><br>
+
+<input type="submit" name="login"value="LOGIN"><br>
+<button>SUBMIT</button>
+</form>
+
+</html>
